9176cccebbb0d2e038bfc31db090e83105ae7ca8
anderson <carrotgoldmlp@gmail.com>
2025-09-08 17:05:24 -0400

    fixed release-web.sh

--- release-web.sh
+++ release-web.sh
@@ -1,6 +1,3 @@
-# (One time) Login to Docker Hub
-docker login -u "tuplasma"
-
 # 1. Build the image locally
 
 # 2. Get the current Git version
@@ -8,9 +5,9 @@ GIT_HASH=$(git rev-parse --short HEAD)
 echo "Tagging version: $GIT_HASH"
 
 # 3. Tag the image for release
-sudo docker tag supabase-web  tuplasma/regrade-web:$GIT_HASH
-sudo docker tag regrade-web tuplasma/regrade-web:latest
+docker tag supabase-web  tuplasma/regrade-web:$GIT_HASH
+docker tag regrade-web tuplasma/regrade-web:latest
 
 # 4. Push to Docker Hub
-sudo docker push tuplasma/regrade-web:$GIT_HASH
-sudo docker push tuplasma/regrade-web:latest
+docker push tuplasma/regrade-web:$GIT_HASH
+docker push tuplasma/regrade-web:latest
